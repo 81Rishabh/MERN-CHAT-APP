75da800c3dbbc5739a62e27f5aa90635fb6a01fb
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2023-02-22 14:44:52 +0530

    Add product url

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,5 @@
 import {io} from 'socket.io-client';
-const ENDPOINT = 'http://localhost:8080';
+// const ENDPOINT = 'http://localhost:8080';
+const PRODUCT_ENDPOINT = 'https://chitchat-nio0.onrender.com';
 
-export const socket = io(ENDPOINT);
+export const socket = io(PRODUCT_ENDPOINT);
